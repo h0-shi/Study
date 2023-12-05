7bfd1c6e3e261c29788929bc936dafa75875c328
Galric <whdgus9655@naver.com>
2023-12-05 09:18:29 +0900

    dec05 추가

---
+++ dec05/src/dec05/HelloWorld.java
@@ -0,0 +1,13 @@
+package dec05;
+
+public class HelloWorld {
+
+	public static void main(String[] args) { // 클래스
+		//변수 선언
+		int number = 123;
+		System.out.println(number);
+		System.out.println("Hello World");
+		
+	}
+
+}
