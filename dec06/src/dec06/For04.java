a3dc025b612b050deb404f265bb0033ec493e583
Galric <whdgus9655@naver.com>
2023-12-06 17:46:08 +0900

    23.12.06 수업 끝

--- dec06/src/dec06/For04.java
+++ dec06/src/dec06/For04.java
@@ -18,6 +18,20 @@ public class For04 {
 			}
 			System.out.println("");
 		}
+		int number[] = {10,20,30,40,50};
+		for(int i = 0; i < number.length; i++) {
+			System.out.println(number[i]);
+		}
+		for(int i : number) { // : 데이터 : 자료
+			System.out.println(i);
+		}
+		System.out.println("===절취선===");
+		for (char ch = 'a'; ch<'f';ch++) { // 숫자 없이 for문 돌리기
+			System.out.println(ch);
+		}
+		for(;;) {	//무한루프
+			System.out.println("나는 네모다");
+		}
 	}
 
 }
