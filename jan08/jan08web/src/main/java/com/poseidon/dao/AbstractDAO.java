725e803924f9a30a96ebc6bf7629b223a764a10a
h0-shi <whdgus9655@naver.com>
2024-01-12 17:00:49 +0900

    로그인 기능 구현

---
+++ jan08/jan08web/src/main/java/com/poseidon/dao/AbstractDAO.java
@@ -0,0 +1,38 @@
+package com.poseidon.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.poseidon.db.DBConnection;
+
+public class AbstractDAO {
+	
+	DBConnection db = DBConnection.getInstance();
+	
+	protected void close(ResultSet rs, PreparedStatement pstmt, Connection con) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if(pstmt != null) {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		if(con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
